c8b2fbaf326fedd333d230a886f7d6426998afdb
Ferret-Master <64487611+Ferret-Master@users.noreply.github.com>
2020-09-03 15:25:05 +0800

    Update AutoFactoryTime.js
    commented out unnecessary console log

--- ui/mods/Autofactory/live_game/AutoFactoryTime.js
+++ ui/mods/Autofactory/live_game/AutoFactoryTime.js
@@ -1,7 +1,7 @@
 //Only purpose is to relay time data to live_game
 function SendTime(){
 	var CurrentTime = model.currentTimeInSeconds()
-	console.log("current time in time bar is "+CurrentTime)
+	//console.log("current time in time bar is "+CurrentTime)
 
 	api.Panel.message(api.Panel.parentId, 'AFtime',CurrentTime)
 	setTimeout(SendTime, 1000);
